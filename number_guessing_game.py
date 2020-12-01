c84de99641420fcd462e16e50240a8b139d5439b
Ubuntu <ubuntu@ip-172-31-1-183.ec2.internal>
2020-12-01 22:43:27 +0000

    commit message

---
+++ number_guessing_game.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+# Created by Sean McLeod
+# Created on November 2020
+# This program is the number guessing game
+
+import random
+
+
+def main():
+    # this function can tell the user if the number they guessed is correct
+
+    # input
+    guessed_number = int(input("Guess a number between 0 to 9: "))
+    print("")
+
+    # process
+    random_number = random.randint(0, 9)  # a number between 0 and 9
+
+    # process & output
+    if guessed_number == random_number:
+        print("You are correct!! {} is the correct number."
+              .format(random_number))
+    else:
+        print("You are wrong! {} was the correct number. Try agian."
+              .format(random_number))
+
+
+if __name__ == "__main__":
+    main()
